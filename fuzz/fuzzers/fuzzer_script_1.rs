7e85ab23d4acab936a55532c4f637baedab7f1e7
Minijackson <minijackson@riseup.net>
2017-04-20 14:24:10 +0200

    Update fuzzer script to previous commit's changes

--- fuzz/fuzzers/fuzzer_script_1.rs
+++ fuzz/fuzzers/fuzzer_script_1.rs
@@ -9,7 +9,7 @@ fuzz_target!(|data: &[u8]| {
     if let Ok(ref s) = str::from_utf8(data) {
         if let Ok(exprs) = inf_4301a::parse_expressions(&s) {
             if let Err(err) = inf_4301a::do_the_thing(exprs, &mut inf_4301a::env::Environment::new()) {
-                inf_4301a::error::print_error("<fuzzer>", &s, err);
+                inf_4301a::error::print_error("<fuzzer>", &s, &err);
             }
         }
     }
